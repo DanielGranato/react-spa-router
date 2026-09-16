37d5417e1900fcf0997e2af38d7fc64f4657f600
Daniel Granato <dgmgranato@gmail.com>
2026-09-16 18:56:31 -0400

    add protected route

--- src/main.jsx
+++ src/main.jsx
@@ -6,22 +6,28 @@ import { Register } from './pages/Register/index.jsx'
 import { Feed } from './pages/Feed/index.jsx'
 import { BlogPost } from './pages/BlogPost/index.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
+import { ProtectedRoute } from './components/ProtectedRoute/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-
-        <Route path="/auth">
-          <Route path="register" element={<Register />} />
-          <Route path="login" element={<Login />} />
+        <Route path='/auth'>
+          <Route path='register' element={<Register />} />
+          <Route path='login' element={<Login />} />
         </Route>
-
-        <Route path="/">
-          <Route index element={<Feed />} />
-          <Route path="blog-post" element={<BlogPost />} />
+        <Route path='/'>
+          <Route path='' element={
+            <ProtectedRoute>
+              <Feed />
+            </ProtectedRoute>
+          } />
+          <Route path='blog-post' element={
+            <ProtectedRoute>
+              <BlogPost />
+            </ProtectedRoute>
+          } />
         </Route>
-
       </Routes>
     </BrowserRouter>
   </StrictMode>,
